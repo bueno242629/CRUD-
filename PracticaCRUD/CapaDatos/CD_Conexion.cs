a9cca3ee6978b30bff7f7f5bd8db0d27fffd7b9e
Yoan Josue Bueno Estevez <67972979+bueno242629@users.noreply.github.com>
2021-12-31 00:42:23 -0400

    RegistroVentas

---
+++ PracticaCRUD/CapaDatos/CD_Conexion.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace CapaDatos
+{
+    public class CD_Conexion
+    {
+
+            private SqlConnection Conexion = new SqlConnection("Server=(local);DataBase= Practica;Integrated Security=true");
+
+            public SqlConnection AbrirConexion()
+            {
+                if (Conexion.State == ConnectionState.Closed)
+                    Conexion.Open();
+                return Conexion;
+            }
+
+            public SqlConnection CerrarConexion()
+            {
+                if (Conexion.State == ConnectionState.Open)
+                    Conexion.Close();
+                return Conexion;
+            }
+        
+    }
+}
---
+++ PracticaCRUD/CapaDatos/CD_Productos.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace CapaDatos
+{
+    public class CD_Productos
+    {
+
+        private CD_Conexion conexion = new CD_Conexion();
+        SqlDataReader leer;
+        DataTable tabla = new DataTable();
+        SqlCommand comando = new SqlCommand();
+        public DataTable Mostrar()
+        {
+
+            comando.Connection = conexion.AbrirConexion();
+            comando.CommandText = "MostrarProductos";
+            comando.CommandType = CommandType.StoredProcedure;
+            leer = comando.ExecuteReader();
+            tabla.Load(leer);
+            conexion.CerrarConexion();
+            return tabla;
+
+        }
+        public void Insertar(string nombre, string desc, string marca, double precio, int stock)
+        {
+            comando.Connection = conexion.AbrirConexion();
+            comando.CommandText = "InsetarProductos";
+            comando.CommandType = CommandType.StoredProcedure;
+            comando.Parameters.AddWithValue("@nombre", nombre);
+            comando.Parameters.AddWithValue("@descrip", desc);
+            comando.Parameters.AddWithValue("@Marca", marca);
+            comando.Parameters.AddWithValue("@precio", precio);
+            comando.Parameters.AddWithValue("@stock", precio);
+            comando.ExecuteNonQuery();
+            comando.Parameters.Clear();
+            conexion.CerrarConexion();
+        }
+        public void Editar(string nombre, string desc, string marca, double precio, int stock, int id)
+        {
+            comando.Connection = conexion.AbrirConexion();
+            comando.CommandText = "EditarProductos";
+            comando.CommandType = CommandType.StoredProcedure;
+            comando.Parameters.AddWithValue("@nombre", nombre);
+            comando.Parameters.AddWithValue("@descrip", desc);
+            comando.Parameters.AddWithValue("@Marca", marca);
+            comando.Parameters.AddWithValue("@precio", precio);
+            comando.Parameters.AddWithValue("@stock", precio);
+            comando.Parameters.AddWithValue("@id", id);
+            comando.ExecuteNonQuery();
+            comando.Parameters.Clear();
+            conexion.CerrarConexion();
+        }
+        public void Eliminar(int id)
+        {
+            comando.Connection = conexion.AbrirConexion();
+            comando.CommandText = "EliminarProducto";
+            comando.CommandType = CommandType.StoredProcedure;
+            comando.Parameters.AddWithValue("@idpro", id);
+            comando.ExecuteNonQuery();
+            comando.Parameters.Clear();
+            conexion.CerrarConexion();
+        }
+    }
+}
